463cf6c3eb5985cf11a7fbf7578a1b921537dad0
root <root@ip-172-31-16-221.us-east-2.compute.internal>
2018-08-21 16:23:19 +0000

    SG Changes

--- sg_allow_ssh.tf
+++ sg_allow_ssh.tf
@@ -5,16 +5,16 @@ resource "aws_security_group" "sg_allow_ssh" {
 
 	egress {
 		cidr_blocks = [ "0.0.0.0/0" ]
-		from_port = 0
-		to_port = 0
-		protocol = "ssh"
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
 	}
 
 	ingress {
 		cidr_blocks = [ "0.0.0.0/0" ]
 		from_port = 22
 		to_port = 22
-		protocol = "ssh"
+		protocol = "tcp"
 	}
 
 	tags {
